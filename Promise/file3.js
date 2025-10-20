da9af30508f3d9b87b5d3b6aff97d20bfffb1b14
ali harit <111861191+itsaliiiiiiii@users.noreply.github.com>
2025-10-20 10:54:11 +0100

    Promise

--- Promise/file3.js
+++ Promise/file3.js
@@ -1,20 +1,46 @@
-console.log("Program Started")
+// console.log("Program Started")
 
-const promise = new Promise((res, rej) => {
-    setTimeout(() => {
-        console.log("1");
-        res();
-    }, 3000);
-});
+// const promise = new Promise((res, rej) => {
+//     setTimeout(() => {
+//         console.log("1");
+//         res();
+//     }, 3000);
+// });
 
 
-console.log(promise)
-console.log("in progress")
-promise.then(() => {
-    return new Promise((res,rej)=>{
-        console.log("2")
-        setTimeout(res,3000)
-    });
-}).then(() => {
-    console.log("3")
-})
+// console.log(promise)
+// console.log("in progress")
+// promise.then((res, rej) => {
+//     console.log("2")
+//     setTimeout(res, 3000)
+// })
+//     .then((res, rej) => {
+//         console.log("3")
+//         setTimeout(res, 3000)
+//     })
+//     .then((res, rej) => {
+//         console.log("4")
+//         setTimeout(res, 3000)
+//     })
+//     .then((res, rej) => {
+//         console.log("5")
+//         setTimeout(res, 3000)
+//     })
+//     .catch((res, rej) => {
+//         console.log("6")
+//         setTimeout(res, 3000)
+//     })
+//     .then((res, rej) => {
+//         console.log("7")
+//         setTimeout(res, 3000)
+//     })
+
+
+console.log("ghjk")
+setTimeout(()=>[
+    console.log("kkkk")
+],1)
+for (let index = 0; index < 100000; index++) {
+    console.log("dfghj")
+}
+console.log("ghjk")
