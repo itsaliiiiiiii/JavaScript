9e6c6395cfcf5d35f3bcaa6511f4bd8d63340558
ali harit <111861191+itsaliiiiiiii@users.noreply.github.com>
2025-10-20 11:45:12 +0100

    finish tp1 Node js

--- Node.js/index.js
+++ Node.js/index.js
@@ -1,12 +1,20 @@
+
 const http = require("node:http");
-const server = http.serve((req, res) => {
-    res.writeHead({ "Content-Type": "text/html" })
-    if (req.url === "/home") {
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200,{ "Content-Type": "text/html" })
+    if (req.url === "/") {
         res.write("<h1>Hello Home</h1>")
     }
     else if (req.url === "/contact") {
         res.write("<h1>Hello Contact</h1>")
     } else {
         res.write("<h1>404</h1>")
-    }
+    } 
+})
+
+
+
+server.listen(3000, () => {
+    console.log("running")
 })
